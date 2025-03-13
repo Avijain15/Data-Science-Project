27cb68a6fe5806ef899a5f0fbe30a10741370c32
Avijain15 <akshat.lallan678@gmail.com>
2025-03-13 11:46:43 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    """ This function will return the list of requirements from the given file path """
+    requirements=[]
+    with open(file_path) as file_obj:
+
+
+
+
+
+
+setup(
+    name="Data Science Project",
+    version="0.0.1",
+    author="Akshat Lallan",
+    author_email="akshat.lallan678@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
