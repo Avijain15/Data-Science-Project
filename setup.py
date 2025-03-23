c0d881d4df79bbe3bb357ef7a8a99190088cd62c
Avijain15 <akshat.lallan678@gmail.com>
2025-03-23 14:49:22 +0530

    EDA and Problems Status

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
+hyphen="-e ."
 def get_requirements(file_path:str)->List[str]:
     """ This function will return the list of requirements from the given file path """
     requirements=[]
     with open(file_path) as file_obj:
-
-
-
-
+        requirements=file_obj.readlines()
+        requirements=[req.replace ("\n","") for req in requirements]
+        if hyphen in requirements:
+            requirements.remove(hyphen)
+    return requirements
 
 
 setup(
@@ -16,5 +18,5 @@ setup(
     author="Akshat Lallan",
     author_email="akshat.lallan678@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt'),
+    install_requires=get_requirements('requirements.txt')
 )
